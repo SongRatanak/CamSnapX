acad6c14d04546355b321680ef2eb1c2837d46b4
song.ratanak <song.ratanak@rupp.edu.kh>
2026-03-13 14:55:09 +0700

    Initial Commit

---
+++ CamSnapX/CamSnapXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamSnapXApp.swift
+//  CamSnapX
+//
+//  Created by SongRatanak on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CamSnapXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
